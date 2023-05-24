08b30987ca2de8f5f603b4376d6ce38aa2763b01
nates linux desktop <nhapeman@gmail.com>
2023-05-23 20:17:43 -0700

    Fix broken format command and run it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './reset.scss';
 import './App.scss';
-import TicTacToe from './tic-tac-toe/TicTacToe'
-import Calculator from './calculator/Calculator'
-import CardMemoryGame from './card-memory-game/CardMemoryGame'
-import Sidebar from "./sidebar/Sidebar"
+import TicTacToe from './tic-tac-toe/TicTacToe';
+import Calculator from './calculator/Calculator';
+import CardMemoryGame from './card-memory-game/CardMemoryGame';
+import Sidebar from './sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Sidebar></Sidebar>
       <main>
         <ul>
-          <li><TicTacToe></TicTacToe></li>
-          <li><Calculator></Calculator></li>
-          <li><CardMemoryGame></CardMemoryGame></li>
+          <li>
+            <TicTacToe></TicTacToe>
+          </li>
+          <li>
+            <Calculator></Calculator>
+          </li>
+          <li>
+            <CardMemoryGame></CardMemoryGame>
+          </li>
         </ul>
       </main>
     </div>
