3ada6062e63c67f35be38b53048dd3d084156d8f
nates linux desktop <nhapeman@gmail.com>
2023-05-27 18:26:50 -0700

    Get some basic tic tac toe logic in.

--- src/tic-tac-toe/TicTacToe.tsx
+++ src/tic-tac-toe/TicTacToe.tsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../Card';
 import './TicTacToe.scss';
+import styled from 'styled-components';
+
+const Board = styled.div`
+  display: grid;
+  grid-template-columns: 2em 2em 2em;
+  grid-gap: 1px;
+`;
+
+const Cell = styled.button`
+  aspect-ratio: 1;
+  background-color: red;
+`;
 
 export default function TicTacToe() {
-  let xsTurn = true;
-  const history = new Array(9).fill(null);
+  type MoveOptions = 'x' | 'o';
+  type CellOptions = MoveOptions | null;
+  const [grid, setGrid] = useState<CellOptions[]>(Array<CellOptions>(9).fill(null));
+  const [moves, setMoves] = useState<MoveOptions[]>([]);
+  const xsTurn = moves.length % 2 == 0;
+  const whoseTurn = xsTurn ? 'x' : 'o';
+
   const reset = () => {
-    history.fill(null);
+    grid.fill(null);
+    setGrid([...grid]);
+    setMoves([]);
   };
+
+  const isWinner = (): boolean => {
+    console.log('TODO: find winner.');
+    return false;
+  };
+
+  const cellClick = (index: number) => {
+    grid[index] = whoseTurn;
+    setGrid([...grid]);
+    setMoves([...moves, whoseTurn]);
+    if (isWinner()) {
+    }
+  };
+
   return (
     <Card header='Tic-Tac-Toe'>
       <section className='Tic-Tac-Toe'>
-        <p>Your move {xsTurn ? 'x' : 'o'}</p>
-        {history.map((h) => {
-          return <div className='cell'>{h}</div>;
-        })}
+        <p>Your move {whoseTurn}</p>
+        <button onClick={reset}>Reset</button>
+        <Board>
+          {grid.map((h, index) => {
+            return (
+              <Cell key={index} onClick={() => cellClick(index)}>
+                {h}
+              </Cell>
+            );
+          })}
+        </Board>
       </section>
     </Card>
   );
