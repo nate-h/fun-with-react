07d56ebc1216a9612dc8a36a627628b8987fd5e3
nates linux desktop <nhapeman@gmail.com>
2023-05-27 22:06:09 -0700

    Winning logic works.

--- src/tic-tac-toe/TicTacToe.tsx
+++ src/tic-tac-toe/TicTacToe.tsx
@@ -3,30 +3,51 @@ import clsx from 'clsx';
 import Card from '../Card';
 import './TicTacToe.scss';
 
+type CellOptions = 'x' | 'o' | ' ';
+
+const findWinner = (grid: CellOptions[]): CellOptions => {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const winningCombo of winningCombos) {
+    const [a, b, c] = winningCombo.map((index) => grid[index]);
+    if (a === b && a === c && a === 'x') {
+      return 'x';
+    }
+    if (a === b && a === c && a === 'o') {
+      return 'o';
+    }
+  }
+  return ' ';
+};
+
 export default function TicTacToe() {
-  type MoveOptions = 'x' | 'o';
-  type CellOptions = MoveOptions | null;
-  const [grid, setGrid] = useState<CellOptions[]>(Array<CellOptions>(9).fill(null));
-  const [moves, setMoves] = useState<MoveOptions[]>([]);
+  const [grid, setGrid] = useState<CellOptions[]>(Array<CellOptions>(9).fill(' '));
+  const [moves, setMoves] = useState<CellOptions[]>([]);
   const xsTurn = moves.length % 2 === 0;
   const whoseTurn = xsTurn ? 'x' : 'o';
+  const [gameOver, setGameOver] = useState<boolean>(false);
 
   const reset = () => {
-    grid.fill(null);
+    grid.fill(' ');
     setGrid([...grid]);
     setMoves([]);
-  };
-
-  const isWinner = (): boolean => {
-    console.log('TODO: find winner.');
-    return false;
   };
 
   const cellClick = (index: number) => {
     grid[index] = whoseTurn;
     setGrid([...grid]);
     setMoves([...moves, whoseTurn]);
-    if (isWinner()) {
+    let winner;
+    if ((winner = findWinner(grid) !== ' ')) {
+      setGameOver(true);
     }
   };
 
@@ -36,15 +57,15 @@ export default function TicTacToe() {
         <p>Your move {whoseTurn}</p>
         <button onClick={reset}>Reset</button>
         <div className='board'>
-          {grid.map((h, index) => {
+          {grid.map((c, index) => {
             return (
               <button
                 key={index}
                 onClick={() => cellClick(index)}
-                className={clsx({ active: grid[index] == null })}
-                disabled={grid[index] != null}
+                className={clsx({ active: grid[index] === ' ' })}
+                disabled={grid[index] !== ' ' || gameOver}
               >
-                {h}
+                {c}
               </button>
             );
           })}
