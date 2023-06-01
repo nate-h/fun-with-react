de86def071a08d9f40f8493892d22ac38060586d
nates linux desktop <nhapeman@gmail.com>
2023-06-01 11:27:00 -0700

    Stop watch works.

--- src/stop-watch/StopWatch.tsx
+++ src/stop-watch/StopWatch.tsx
@@ -3,56 +3,79 @@ import clsx from 'clsx';
 import Card from '../Card';
 import './Stopwatch.scss';
 
+function dt(t1: Date, t2: Date): number {
+  return t2.getTime() - t1.getTime();
+}
+
+function formatDisplay(dt: number): string {
+  const zeroPad = (num: number) => String(num).padStart(2, '0');
+  const ms = Math.floor((dt % 1000) / 10);
+  const secs = Math.floor((dt / 1000) % 60);
+  const mins = Math.floor((dt / 1000 / 60) % 60);
+  return `${zeroPad(mins)}:${zeroPad(secs)}:${zeroPad(ms)}`;
+}
+
 export default function Stopwatch() {
-  const [startTime, setStartTime] = useState<Date | null>(null);
-  const [times, setTimes] = useState<String[]>([]);
-  const [lastTimeMS, setLastTimeMS] = useState<number>(0);
+  const [startTime, setStartTime] = useState<Date>(new Date());
+  const [totalTimeDelta, setTotalTimeDelta] = useState<number>(0);
+  const [lapStartTime, setLapStartTime] = useState<Date>(new Date());
+  const [lapTimeDelta, setLapTimeDelta] = useState<number>(0);
+  const [now, setNow] = useState<Date>(new Date());
+  const [lapTimes, setLapTimes] = useState<number[]>([]);
   const currentTimer = useRef<number>(0);
+  const totalDisplay = formatDisplay(totalTimeDelta + dt(startTime, now));
+  const lapDisplay = formatDisplay(lapTimeDelta + dt(lapStartTime, now));
+  let slowestLap = -1;
+  let fastestLap = -1;
+  if (lapTimes.length > 1) {
+    slowestLap = lapTimes.indexOf(Math.max(...lapTimes));
+    fastestLap = lapTimes.indexOf(Math.min(...lapTimes));
+  }
+
+  const setTimes = (newNow: Date) => {
+    setStartTime(newNow);
+    setLapStartTime(newNow);
+    setNow(newNow);
+  };
 
   const startTimer = () => {
-    setStartTime(new Date());
+    setTimes(new Date());
     currentTimer.current = window.setInterval(() => {
-      setTimes([...times, Date.now().toLocaleString()]);
+      setNow(new Date());
       console.log('.');
     }, 1 / 30);
   };
   const endTimer = () => {
-    setStartTime(null);
-    setLastTimeMS(lastTimeMS + timeDiffMS());
+    const now_ = new Date();
+    setTimes(now_);
+    setTotalTimeDelta(totalTimeDelta + dt(startTime, now_));
+    setLapTimeDelta(lapTimeDelta + dt(lapStartTime, now_));
     window.clearInterval(currentTimer.current);
     currentTimer.current = 0;
   };
 
   const reset = () => {
-    setLastTimeMS(0);
-    setTimes([]);
+    setTimes(new Date());
+    setTotalTimeDelta(0);
+    setLapTimeDelta(0);
+    setLapTimes([]);
   };
 
   const lap = () => {
-    console.log('lap');
+    setLapTimes([...lapTimes, lapTimeDelta + dt(lapStartTime, new Date())]);
+    setLapTimeDelta(0);
+    setLapStartTime(new Date());
   };
 
-  const timeDiffMS = () => {
-    if (startTime) {
-      return new Date().getTime() - startTime.getTime();
-    }
-    return 0;
-  };
-
-  const displayTime = () => {
-    const zeroPad = (num: number) => String(num).padStart(2, '0');
-    const raw = lastTimeMS + timeDiffMS();
-    const ms = Math.floor((raw % 1000) / 10);
-    const secs = Math.floor((raw / 1000) % 60);
-    const mins = Math.floor((raw / 1000 / 60) % 60);
-    return `${zeroPad(mins)}:${zeroPad(secs)}:${zeroPad(ms)}`;
+  const lapStyling = (index: number) => {
+    return clsx({ slowest: index === slowestLap, fastest: index === fastestLap });
   };
 
   return (
     <Card header='Stopwatch' subheader='Modeled after iPhone Stopwatch'>
       <section className='Stopwatch'>
-        <p>{displayTime()}</p>
-        {startTime ? (
+        <p>{totalDisplay}</p>
+        {currentTimer.current ? (
           <div>
             <button onClick={lap}>Lap</button>
             <button onClick={endTimer} className='stop'>
@@ -68,6 +91,18 @@ export default function Stopwatch() {
           </div>
         )}
         <hr />
+        <ul className='lap-times'>
+          {lapTimes.map((dt, i) => {
+            return (
+              <li key={i} className={lapStyling(i)}>
+                <div className='spacer'>Lap {i + 1}</div> {formatDisplay(dt)}
+              </li>
+            );
+          })}
+          <li>
+            <div className='spacer'>Lap {lapTimes.length + 1}</div> {lapDisplay}
+          </li>
+        </ul>
       </section>
     </Card>
   );
