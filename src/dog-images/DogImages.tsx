4b83e85c2481137d369261071eaf523542cffb14
nates linux desktop <nhapeman@gmail.com>
2023-06-07 23:20:25 -0700

    cosmetics on dog image slider.

--- src/dog-images/DogImages.tsx
+++ src/dog-images/DogImages.tsx
@@ -24,40 +24,39 @@ export default function DogImages() {
     myFetch(`https://dog.ceo/api/breed/${breed}/images`, setBreedLinks);
   };
 
+  const adjustPicIndex = (delta: number) => {
+    const newPicIndex = picIndex + delta;
+    setPicIndex(Math.min(breedLinks.length - 1, Math.max(0, newPicIndex)));
+  };
+
   return (
     <Card header='DogImages'>
       <section className='DogImages'>
-        <p>Pick a dog breed to see a pic.</p>
-        <p>
-          Credit to <a href='https://dog.ceo/dog-api/'>the dog api</a> for the backend.
-        </p>
-        <p className='todo'>use index-db</p>
-        <p className='todo'>handle throw better</p>
-        <br />
-        Select a breed:
-        {breeds ? (
-          <select onChange={breedChange} value={breed}>
-            {breeds.map((b) => (
-              <option key={b}>{b}</option>
-            ))}
-          </select>
-        ) : null}
-        {breed ? (
-          <>
-            <br />
-            <br />
-            {picIndex + 1} / {breedLinks.length}
-            &nbsp;
-            <button onClick={() => setPicIndex(Math.max(0, picIndex - 1))}>Prev</button>
-            <button onClick={() => setPicIndex(Math.min(breedLinks.length - 1, picIndex + 1))}>
-              Next
-            </button>
-            <br />
-            <br />
-            <br />
-            <br />
-          </>
-        ) : null}
+        <aside>
+          <p>Pick a dog breed to see a pic.</p>
+          <p>
+            Credit to <a href='https://dog.ceo/dog-api/'>the dog api</a> for the backend.
+          </p>
+          <p className='todo'>use index-db</p>
+          <p className='todo'>handle throw better</p>
+          <br />
+          Breed:&nbsp;
+          {breeds ? (
+            <select onChange={breedChange} value={breed}>
+              {breeds.map((b) => (
+                <option key={b}>{b}</option>
+              ))}
+            </select>
+          ) : null}
+          {breed ? (
+            <div>
+              {picIndex + 1} / {breedLinks.length}
+              &nbsp;
+              <button onClick={() => adjustPicIndex(-1)}>Prev</button>
+              <button onClick={() => adjustPicIndex(1)}>Next</button>
+            </div>
+          ) : null}
+        </aside>
         {imageURL ? <img src={imageURL} alt={`Random pic: ${imageURL}`} /> : null}
       </section>
     </Card>
